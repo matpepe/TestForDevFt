8d32c79f5d2d6df3d5de72ba8207d288de8f9c0b
matpepe <125389896+matpepe@users.noreply.github.com>
2024-01-28 19:49:27 +0100

    CommitWorkingToiiS

--- TestWebAppMulti/wwwroot/js/translation.js
+++ TestWebAppMulti/wwwroot/js/translation.js
@@ -1,24 +1,24 @@
 ﻿// translation.js
 
-//function googleTranslateElementInit() {
-//    new google.translate.TranslateElement(
-//        {
-//            pageLanguage: 'en', // Default language
-//            includedLanguages: 'en,hr,sr,sl,de,ru,zh-CN', // English, Croatian, Serbian, Slovenian, German, Russian, Chinese
-//            layout: google.translate.TranslateElement.InlineLayout.SIMPLE, // or other layout as needed
-//            autoDisplay: true
-//        },
-//        'google_translate_element'
-//    );
-//}
-
-
 function googleTranslateElementInit() {
-    new google.translate.TranslateElement({
-        pageLanguage: 'en',
-        includedLanguages: 'es,fr,de,ja,zh-CN',
-        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-        gaTrack: true,
-        gaId: 'YOUR_GA_ID' // Replace with your Google Analytics ID
-    }, 'google_translate_element');
+    new google.translate.TranslateElement(
+        {
+            pageLanguage: 'en', // Default language
+            includedLanguages: 'en,hr,sr,sl,de,ru,zh-CN', // English, Croatian, Serbian, Slovenian, German, Russian, Chinese
+            layout: google.translate.TranslateElement.InlineLayout.SIMPLE, // or other layout as needed
+            autoDisplay: true
+        },
+        'google_translate_element'
+    );
 }
+
+
+//function googleTranslateElementInit() {
+//    new google.translate.TranslateElement({
+//        pageLanguage: 'en',
+//        includedLanguages: 'es,fr,de,ja,zh-CN',
+//        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+//        gaTrack: true,
+//        gaId: 'YOUR_GA_ID' // Replace with your Google Analytics ID
+//    }, 'google_translate_element');
+//}
